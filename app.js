5d60608f2fc7dd74cbd21b4cd3de34b6dcefc9b7
Ozan Aksoy <e233015@metu.edu.tr>
2022-02-17 18:13:22 +0300

    weatherapp

---
+++ app.js
@@ -0,0 +1,34 @@
+const url = 'https://api.openweathermap.org/data/2.5/'
+const key = '97cb48a53856db8ceed3a2b25f859670'
+
+const setQuery = (e) => {
+    if (e.keyCode == '13')
+        getResult(searchBar.value)
+}
+
+const getResult = (cityName) => {
+    let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=en`
+    fetch(query)
+        .then(weather => {
+            return weather.json()
+        })
+        .then(displayResult)
+}
+
+const displayResult = (result) => {
+    let city = document.querySelector('.cityName')
+    city.innerText = `${result.name}, ${result.sys.country}`
+
+    let temp = document.querySelector('.temp')
+    temp.innerText = `${Math.round(result.main.temp)}°C`
+
+    let desc = document.querySelector('.description')
+    desc.innerText = result.weather[0].description;
+
+    let minmax = document.querySelector('.minmax')
+    minmax.innerText = `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`
+}
+
+
+const searchBar = document.getElementById('searchBar')
+searchBar.addEventListener('keypress', setQuery)
